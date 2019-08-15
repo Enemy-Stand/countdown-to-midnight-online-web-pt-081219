36f87825d88e9cb1ebdbeeaf107cb8392e3c5946
Joseph Sesti <sestijoey63@gmail.com>
2019-08-15 19:23:57 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -16,8 +16,9 @@ def countdown_with_sleep(number)
   while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
+    sleep 1
   end
   return "HAPPY NEW YEAR!"
 end
 
-countdown_with_sleep
+countdown_with_sleep(10)
